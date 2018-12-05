c07087747623a8722f945ac3df86af88dbd7e54d
geekerstar <247507792@qq.com>
2018-12-05 17:22:37 +0800

    配置熔断器

---
+++ verynavi_qa/src/main/java/com/verynavi/qa/client/impl/LabelClientImpl.java
@@ -0,0 +1,20 @@
+package com.verynavi.qa.client.impl;
+
+import com.verynavi.qa.client.LabelClient;
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author geekerstar
+ * @date 2018/12/5
+ * description
+ */
+@Component
+public class LabelClientImpl implements LabelClient {
+
+    @Override
+    public Result findById(String labelId) {
+        return new Result(false, StatusCode.ERROR,"熔断器触发了！");
+    }
+}
